c450391ca28de5804a77d8a5da77cdbfc259d46b
cash123tw <65075198+cash123tw@users.noreply.github.com>
2023-08-01 12:41:48 +0800

    test

--- src/main/java/annotation/Data.java
+++ src/main/java/annotation/Data.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target({ElementType.TYPE,ElementType.FIELD})
-@Retention(RetentionPolicy.SOURCE)s
+@Retention(RetentionPolicy.SOURCE)
 public @interface Data {
 }
