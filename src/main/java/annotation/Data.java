92d6c7b5e1f5f6425396ab6b2725bea4cd262130
Cash Li KS <cash123tw@gmail.com>
2023-08-01 13:09:07 +0800

    XTe

--- src/main/java/annotation/Data.java
+++ src/main/java/annotation/Data.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target({ElementType.TYPE,ElementType.FIELD})
-@Retention(RetentionPolicy.SOURCE)xxxxxx
+@Retention(RetentionPolicy.SOURCE)xxxxxxss
 public @interface Data {
 }
